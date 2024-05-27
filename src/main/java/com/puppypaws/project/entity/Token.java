f91ea7077984d8e31330ef267f99c9de1a0bfa0a
yubin <hyb961121@naver.com>
2024-05-27 10:13:40 +0900

    feat: token generate id

--- src/main/java/com/puppypaws/project/entity/Token.java
+++ src/main/java/com/puppypaws/project/entity/Token.java
@@ -2,18 +2,20 @@ package com.puppypaws.project.entity;
 
 import jakarta.persistence.*;
 import lombok.Data;
-import org.springframework.data.annotation.Id;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 @Entity
 @Data
+@EntityListeners(AuditingEntityListener.class)
 @Table(name = "Token")
 public class Token {
-    @jakarta.persistence.Id
+    
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "token_id_generator")
     @SequenceGenerator(name="token_id_generator", sequenceName="token_id", allocationSize = 1)
     @Column(name = "id")
     private Long id;
+
     @Column(name = "access_token", length = 2048)
     private String accessToken;
 
