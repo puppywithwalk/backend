e7ade6f394e537d6b15db2d105c49d694faed887
정창환 <wjdckdcjd@gmail.com>
2024-07-20 01:19:02 +0900

    fix: Dogstargram 조회 쿼리 개선

--- src/main/java/com/puppypaws/project/repository/DogstagramRepository.java
+++ src/main/java/com/puppypaws/project/repository/DogstagramRepository.java
@@ -48,57 +48,59 @@ public interface DogstagramRepository extends JpaRepository<Dogstagram, Long> {
             "LIMIT :take " +
             "OFFSET :skip", nativeQuery = true)
     public List<IDogstagram> getDogstagramList(@Param(value = "id") Long id, @Param(value = "take") int take, @Param(value = "skip") int skip);
-    @Query(value =
+
+    @Query(value ="WITH latest_likes AS ( " +
+            "    SELECT " +
+            "        dl.dogstagram_id, " +
+            "        m.nickname, " +
+            "        ROW_NUMBER() OVER (PARTITION BY dl.dogstagram_id ORDER BY dl.created_at DESC) AS rn " +
+            "    FROM " +
+            "        dogstagram_like dl " +
+            "            INNER JOIN " +
+            "        member m ON dl.user_id = m.id " +
+            "), " +
+            "     top_dogstagrams AS ( " +
+            "         SELECT " +
+            "             dogstagram_id, " +
+            "             COUNT(*) AS total_like " +
+            "         FROM " +
+            "             dogstagram_like " +
+            "         GROUP BY " +
+            "             dogstagram_id " +
+            "         ORDER BY " +
+            "             total_like DESC " +
+            "         LIMIT 4 " +
+            "     ) " +
             "SELECT " +
-                    "    d.id AS id," +
-                    "    d.description AS description," +
-                    "    a.url AS url," +
-                    "    a.url2 AS url2," +
-                    "    a.url3 AS url3," +
-                    "    m.nickname AS nickname," +
-                    "    m.id AS user_id," +
-                    "    m.dog_type AS dog_type," +
-                    "    m.profile_url AS profile_url," +
-                    "    COALESCE(last_like.nickname, '') AS last_liked_nickname," +
-                    "    d.created_at AS created_at," +
-                    "    COUNT(dl.id) AS total_like," +
-                    "    EXISTS (" +
-                    "        SELECT 1" +
-                    "        FROM dogstagram_like dl2" +
-                    "        WHERE dl2.dogstagram_id = d.id AND dl2.user_id = :id" +
-                    "    ) AS is_liked\n" +
-                    "FROM " +
-                    "    dogstagram d " +
-                    "INNER JOIN " +
-                    "    attachment a ON d.attachment_id = a.id " +
-                    "INNER JOIN " +
-                    "    member m ON d.member_id = m.id " +
-                    "LEFT JOIN ( " +
-                    "    SELECT " +
-                    "        dl.dogstagram_id," +
-                    "        m.nickname," +
-                    "        dl.created_at" +
-                    "    FROM ( " +
-                    "        SELECT " +
-                    "            dogstagram_id, " +
-                    "            MAX(created_at) AS max_created_at " +
-                    "        FROM " +
-                    "            dogstagram_like " +
-                    "        GROUP BY " +
-                    "            dogstagram_id" +
-                    "    ) latest " +
-                    "    INNER JOIN " +
-                    "        dogstagram_like dl ON latest.dogstagram_id = dl.dogstagram_id AND latest.max_created_at = dl.created_at " +
-                    "    INNER JOIN " +
-                    "        member m ON dl.user_id = m.id " +
-                    ") last_like ON d.id = last_like.dogstagram_id " +
-                    "LEFT JOIN " +
-                    "    dogstagram_like dl ON d.id = dl.dogstagram_id " +
-                    "GROUP BY " +
-                    "    d.id, d.description, a.url, a.url2, a.url3, m.nickname, m.id, m.dog_type, m.profile_url, last_like.nickname, d.created_at " +
-                    "ORDER BY " +
-                    "    total_like DESC, d.created_at DESC " +
-                    "LIMIT 4 ", nativeQuery = true)
+            "    d.id AS id, " +
+            "    d.description AS description, " +
+            "    a.url AS url, " +
+            "    a.url2 AS url2, " +
+            "    a.url3 AS url3, " +
+            "    m.nickname AS nickname, " +
+            "    m.id AS user_id, " +
+            "    m.dog_type AS dog_type, " +
+            "    COALESCE(ll.nickname, '') AS last_liked_nickname, " +
+            "    m.profile_url AS profile_url, " +
+            "    d.created_at AS created_at, " +
+            "    COALESCE(td.total_like, 0) AS total_like, " +
+            "    EXISTS ( " +
+            "        SELECT 1 " +
+            "        FROM dogstagram_like dl2 " +
+            "        WHERE dl2.dogstagram_id = d.id AND dl2.user_id = :id " +
+            "    ) AS is_liked " +
+            "FROM " +
+            "    dogstagram d " +
+            "        INNER JOIN " +
+            "    attachment a ON d.attachment_id = a.id " +
+            "        INNER JOIN " +
+            "    member m ON d.member_id = m.id " +
+            "        INNER JOIN " +
+            "    top_dogstagrams td ON d.id = td.dogstagram_id " +
+            "        LEFT JOIN " +
+            "    latest_likes ll ON d.id = ll.dogstagram_id AND ll.rn = 1 " +
+            "ORDER BY " +
+            "    total_like desc", nativeQuery = true)
     public List<IDogstagram> getStarDogstagramList(@Param(value = "id") Long id);
 
     @Query(value =
